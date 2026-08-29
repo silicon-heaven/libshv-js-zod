fdeab79c6f4d8aa3590a06634af44bc12f62913b
Václav Kubernát <sir.venceslas@gmail.com>
2026-08-29 13:29:24 +0200

    Add tests for brand preserving

--- tests/test_zod.ts
+++ tests/test_zod.ts
@@ -1,5 +1,5 @@
 import { fromCpon } from 'libshv-js/cpon';
-import { makeIMap, makeMap, makeMetaMap } from 'libshv-js/rpcvalue';
+import { IMap, isIMap, isShvMap, makeIMap, makeMap, makeMetaMap, ShvMap, shvMapType } from 'libshv-js/rpcvalue';
 import * as z from '../src/zod.js';
 
 z.map({ ok: z.boolean(), })
@@ -23,3 +23,31 @@ const SiteInfoZod = z.recmap(z.map({
 const lol = fromCpon(`{"key": {}}`);
 SiteInfoZod.parse(lol);
 z.rpcvalue().parse(makeIMap({}));
+
+if (z.map({}).parse(makeMap({}))[shvMapType] !== 'map') {
+	throw TypeError('z.map didn\'t preserve brand');
+}
+
+if (z.imap({}).parse(makeIMap({}))[shvMapType] !== 'imap') {
+	throw TypeError('z.imap didn\'t preserve brand');
+}
+
+if (z.recmap(z.any()).parse(makeMap({}))[shvMapType] !== 'map') {
+	throw TypeError('z.recmap didn\'t preserve brand');
+}
+
+if (z.recimap(z.any()).parse(makeIMap({}))[shvMapType] !== 'imap') {
+	throw TypeError('z.recimap didn\'t preserve brand');
+}
+
+z.imap({asdf: z.number()}).parse(makeIMap({asdf: 123})) satisfies IMap;
+z.map({asdf: z.number()}).parse(makeMap({asdf: 123})) satisfies ShvMap;
+
+let some_map = z.map({asdf: z.number()}).parse(makeMap({asdf: 123})) satisfies ShvMap;
+if (!isShvMap(some_map)) {
+	throw TypeError('z.map didn\'t preserve brand');
+}
+let some_imap = z.imap({asdf: z.number()}).parse(makeIMap({asdf: 123})) satisfies IMap;
+if (!isIMap(some_imap)) {
+	throw TypeError('z.imap didn\'t preserve brand');
+}
